ed165473f2385cfca4034045ae9bd4a81ce6dd65
zwforum <782552186@qq.com>
2025-03-21 00:30:48 +0800

    init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,13 +1,4 @@
-from .logs import *
 from .configs import *
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from .logs import *
 
-
-__parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, prog='main.py', add_help=False)
-__parser.add_argument('-c', '--config', type=str, help='path to config file', const=f'configs{os.sep}config.yaml',
-                          metavar='path', default=f'configs{os.sep}config.yaml', nargs='?')
-get_logger()
-get_parser([__parser])
-
-def get_parent_parser():
-    return __parser
+get_parser(get_logger())
