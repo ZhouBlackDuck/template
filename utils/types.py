ac5f1a8f9804a748704a89d73ca050fa4b650dd3
zwforum <782552186@qq.com>
2025-03-21 01:33:32 +0800

    init

---
+++ utils/types.py
@@ -0,0 +1,16 @@
+import re
+
+from jsonargparse.typing import extend_base_type
+
+
+def validation_wrapper(func, msg):
+    def validate(_, v):
+        if not func(v):
+            raise ValueError(msg.format(v))
+
+    return validate
+
+
+FileMode = extend_base_type('FileMode', str,
+                            validation_wrapper(lambda x: re.match(r'^[rwxabt+]+$', x) is not None,
+                                               'Invalid file mode {}'))
