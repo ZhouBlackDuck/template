0c8f16978a66ea4029a289791fa7c7ab010612cb
zwforum <782552186@qq.com>
2025-03-21 02:04:21 +0800

    init

--- utils/logs.py
+++ utils/logs.py
@@ -1,15 +1,23 @@
+import logging
 from logging import getLogger
 
 __logger = None
+DEFAULT_LEVEL = 'INFO'
+DEFAULT_FORMAT = '[%(asctime)s] - %(name)s - [%(levelname)s] - %(message)s'
+DEFAULT_DATEFMT = '%Y-%m-%d %H:%M:%S'
 
 
 def get_logger():
     global __logger
     if __logger is None:
+        logging.basicConfig(level=DEFAULT_LEVEL, format=DEFAULT_FORMAT, datefmt=DEFAULT_DATEFMT)
         __logger = getLogger('main')
     return __logger
 
 
 __all__ = [
-    'get_logger'
+    'get_logger',
+    'DEFAULT_LEVEL',
+    'DEFAULT_FORMAT',
+    'DEFAULT_DATEFMT'
 ]
