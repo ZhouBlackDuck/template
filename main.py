be0471325e84943b9ff284369dddffcd78e6278e
zwforum <782552186@qq.com>
2025-03-21 02:05:10 +0800

    init

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import logging
+
 import utils
 
 if __name__ == '__main__':
     parser = utils.get_parser()
     args = parser.parse_args()
+    logging.basicConfig(**args.logging, force=True)
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,19 @@
+from argparse import SUPPRESS
+from jsonargparse.typing import Path_fr
 from .configs import *
 from .logs import *
+from .types import *
 
 get_parser(get_logger())
+get_parser().add_argument('--logging.filename', help='Path to log file', metavar='path', nargs=1, type=Path_fr,
+                          default=SUPPRESS)
+get_parser().add_argument('--logging.filemode', help='Log file mode', metavar='mode', nargs='?', type=FileMode,
+                          default='a', const='a')
+get_parser().add_argument('--logging.level', help='Logging level', metavar='level', nargs='?',
+                          choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], default=DEFAULT_LEVEL, const=DEFAULT_LEVEL)
+get_parser().add_argument('--logging.format', help='Logging format', metavar='format', nargs='?',
+                          default=DEFAULT_FORMAT, const=DEFAULT_FORMAT)
+get_parser().add_argument('--logging.datefmt', help='Logging date format', metavar='format', nargs='?',
+                          default=DEFAULT_DATEFMT, const=DEFAULT_DATEFMT)
+get_parser().add_argument('--logging.encoding', help='Log file encoding', metavar='encoding', nargs='?',
+                          default='utf-8', const='utf-8')
