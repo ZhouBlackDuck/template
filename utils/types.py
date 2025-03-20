a944c089e4f219c84d3ecf3b7ff3225de21c31e1
zwforum <782552186@qq.com>
2025-03-21 01:34:58 +0800

    init

--- utils/types.py
+++ utils/types.py
@@ -14,3 +14,7 @@ def validation_wrapper(func, msg):
 FileMode = extend_base_type('FileMode', str,
                             validation_wrapper(lambda x: re.match(r'^[rwxabt+]+$', x) is not None,
                                                'Invalid file mode {}'))
+
+__all__ = [
+    'FileMode'
+]
