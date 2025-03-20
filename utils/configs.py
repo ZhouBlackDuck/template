8c93cc8a7710e93b5b620667ae46ba96009dd8b8
zwforum <782552186@qq.com>
2025-03-20 22:45:30 +0800

    init

--- utils/configs.py
+++ utils/configs.py
@@ -1,5 +1,6 @@
 import os
 import os.path as path
+import yaml
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
@@ -8,7 +9,7 @@ __parser = None
 def get_parser():
     global __parser
     if __parser is None:
-        __parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, prog='main')
+        __parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, prog='main.py')
     return __parser
 
 def check_existing_config(config):
@@ -18,3 +19,11 @@ def check_existing_config(config):
     else:
         if not path.exists(path.normpath(path.join(os.getcwd(), config))):
             raise FileNotFoundError(f'Config file {config} does not exist')
+
+def load_config(config):
+    if path.isabs(config):
+        file_path = path.normpath(config)
+    else:
+        file_path = path.normpath(path.join(os.getcwd(), config))
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
