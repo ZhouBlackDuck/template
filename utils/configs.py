117d33c46c04655f1bdb8cea4ed3d27fe36ad6e6
zwforum <782552186@qq.com>
2025-03-21 00:29:56 +0800

    init

--- utils/configs.py
+++ utils/configs.py
@@ -1,31 +1,17 @@
 import os
 import os.path as path
-import yaml
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-
+from logging import Logger
+from jsonargparse import ArgumentParser
 
 __parser = None
 
-def get_parser(parents=None):
+
+def get_parser(logger: [bool, Logger] = False):
     global __parser
     if __parser is None:
-        if parents is None:
-            parents = []
-        __parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter, prog='main.py', parents=parents)
+        __parser = ArgumentParser(prog='main.py', default_env=True, print_config='--print-config',
+                                  default_config_files=[
+                                      path.normpath(path.join(os.getcwd(), 'configs', 'config.yaml')),
+                                  ],
+                                  logger=logger)
     return __parser
-
-def check_existing_config(config):
-    if path.isabs(config):
-        if not path.exists(path.normpath(config)):
-            raise FileNotFoundError(f'Config file {config} does not exist')
-    else:
-        if not path.exists(path.normpath(path.join(os.getcwd(), config))):
-            raise FileNotFoundError(f'Config file {config} does not exist')
-
-def load_config(config):
-    if path.isabs(config):
-        file_path = path.normpath(config)
-    else:
-        file_path = path.normpath(path.join(os.getcwd(), config))
-    with open(file_path, 'r') as file:
-        return yaml.safe_load(file)
