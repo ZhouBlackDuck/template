564ad9ac9821397cc5d8d9110b9cdc0d1e2a81bb
zwforum <782552186@qq.com>
2025-03-21 10:17:37 +0800

    init

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ import utils
 if __name__ == '__main__':
     parser = utils.get_parser()
     args = parser.parse_args()
-    args.logging.filename = args.logging.filename[0].absolute
+    if hasattr(args.logging, 'filename'):
+        args.logging.filename = args.logging.filename[0].absolute
     logging.basicConfig(**args.logging, force=True)
