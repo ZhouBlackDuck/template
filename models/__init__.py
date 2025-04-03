2bc60f4d89eb78a3f1a54df2c4d6ae46c6770c56
zwforum <782552186@qq.com>
2025-04-03 17:30:20 +0800

    init

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,7 @@ import utils
 from jsonargparse import ArgumentParser
 
 __subcommands = utils.get_parser().add_subcommands()
-__parser = ArgumentParser()
-__subcommands.add_subcommand('model', __parser)
+__parsers = dict()
+__parsers['model'] = ArgumentParser()
+for item in __parsers.items():
+    __subcommands.add_subcommand(*item)
