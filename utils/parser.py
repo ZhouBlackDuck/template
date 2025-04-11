e4e481246313f03d12c8bf649d7576a9612e37cc
zwforum <782552186@qq.com>
2025-04-11 17:03:37 +0800

    pass last name

--- utils/parser.py
+++ utils/parser.py
@@ -4,4 +4,4 @@ from jsonargparse import ArgumentParser
 
 
 def pass_dict(parser: ArgumentParser, t: Tuple[str, ...], target: str):
-    parser.link_arguments(t, target, lambda *args: dict(zip(t, args)))
+    parser.link_arguments(t, target, lambda *args: dict(zip([n.split('.')[-1] for n in t], args)))
