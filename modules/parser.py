9b2d09af73950e15f99c59d63de6a9b7c6741b56
zwforum <782552186@qq.com>
2025-04-22 19:15:04 +0800

    Revert "env config file"
    This reverts commit c9875fc47322f85fa91474bf167732e20ac1da33.

--- modules/parser.py
+++ modules/parser.py
@@ -7,17 +7,15 @@ from jsonargparse import ArgumentParser
 
 class ConfigParser:
     _local = threading.local()
-    _env = os.environ.get('DL_ENV', None)
 
     def __new__(cls, *args, **kwargs):
         if getattr(cls._local, 'instance', None) is None:
             cls._local.instance = super().__new__(cls)
-            config_file = f'config.{cls._env}.yaml' if cls._env is not None else 'config.yaml'
             cls._local.instance.parser = ArgumentParser(prog='main.py', default_env=True,
                                                         print_config='--print-config',
                                                         default_config_files=[
                                                             path.normpath(
-                                                                path.join(os.getcwd(), 'configs', config_file)),
+                                                                path.join(os.getcwd(), 'configs', 'config.yaml')),
                                                         ],
                                                         logger='root')
             cls._local.instance.parser.add_argument('-c', '--config', help='Path to config file', metavar='path',
